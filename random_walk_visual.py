a80fd04ca9c73cae373c8479b270467036f9380d
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-27 11:00:07 -0700

    Color the graph to better represent the walk path

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -11,7 +11,17 @@ while True:
     # Plot the points in the walk
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+
+    # Style the plot
+    point_numbers = range(rw.num_points)
+    ax.scatter(
+        rw.x_values, 
+        rw.y_values,
+        c=point_numbers,
+        cmap=plt.cm.Reds,
+        edgecolors='none', 
+        s=15
+        )
     plt.show()
 
     keep_running = input("Do you want to make another walk> (y/n)")
