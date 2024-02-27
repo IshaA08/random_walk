8d37fca07b1985d6911c0176d3e089551f1e6820
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-27 10:56:08 -0700

    Keep making random walks as long as the user wants

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -2,12 +2,18 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Generate a random walk
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making walks as long as the program is active
+while True:
+    # Generate a random walk
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Plot the points in the walk
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    # Plot the points in the walk
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Do you want to make another walk> (y/n)")
+    if keep_running == 'n':
+        break
