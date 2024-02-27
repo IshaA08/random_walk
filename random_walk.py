93c3e25216c92e45247a45858a41d2ea0398cced
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-27 11:21:32 -0700

    Refactor fill_walk method

--- random_walk.py
+++ random_walk.py
@@ -23,13 +23,8 @@ class RandomWalk:
         while len(self.x_values) < self.num_points:
 
             # Decide which direction to go and how far this way
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self._get_step()
+            y_step = self._get_step()
 
             # Reject moves that go nowehere
             if x_step == 0 and y_step == 0:
@@ -38,6 +33,11 @@ class RandomWalk:
             # Calculate and save the new position
             x = self.x_values[-1] + x_step
             y = self.y_values[-1] + y_step
-            
+
             self.x_values.append(x)
             self.y_values.append(y)
+
+    def _get_step(self):
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4, 5])
+        return direction * distance
