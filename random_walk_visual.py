e343e6975aaea778413d81762eebbb151e8aa1d5
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-27 11:04:41 -0700

    Emphasize the start and end points of the walk

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -22,6 +22,16 @@ while True:
         edgecolors='none', 
         s=15
         )
+    
+    # Emphasize the start and end points
+    ax.scatter(0, 0, c='green', edgecolors='none', s=150)
+    ax.scatter(
+        rw.x_values[-1],
+        rw.y_values[-1],
+        c='orange',
+        edgecolors='none',
+        s=150
+    )
     plt.show()
 
     keep_running = input("Do you want to make another walk> (y/n)")
